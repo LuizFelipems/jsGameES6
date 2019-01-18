fe73e1caf5f782b150ad8070d79b15cb5c6e740f
LuizFelipems <luizfelipems12@gmail.com>
2019-01-18 19:21:22 -0300

    Sobre Variáveis.

--- app-src/gameBabel.js
+++ app-src/gameBabel.js
@@ -1,1 +1,22 @@
-console.log('Fui compilado pelo Babel 7, que máximo3! - Compila mesmo automático');
+document.write('Fui compilado pelo Babel 7')
+
+/**
+ * Variáveis
+ * Você usa variáveis como nomes simbólicos para os valores em sua aplicação.
+ * 
+ * Tipos de dados:
+ * Boolean. true e false.
+ * null. Uma palavra-chave que indica valor nulo.
+ * undefined. Uma propriedade superior cujo valor é indefinido.
+ * Number. 42 ou 3.14159.
+ * String. "Howdy"
+ * Symbol (novo em ECMAScript 6). Um tipo de dado cuja as instâncias são únicas e imutáveis.
+ * Object. Objetos
+ */
+ var a = 1
+ /** let - permite que você declare variáveis limitando seu escopo no bloco*/
+ let aa = "1"
+
+ /** Convertendo strings para números */
+//  document.write(parseInt(aa))
+//  document.write(parseFloat(aa))
