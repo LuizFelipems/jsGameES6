e83a40ef694c30ffffa5b150bf41a1e68ee8abf5
LuizFelipems <luizfelipems12@gmail.com>
2019-01-18 23:20:12 -0300

    Sobre Funções

--- app-src/gameBabel.js
+++ app-src/gameBabel.js
@@ -10,12 +10,42 @@
 //  let obj = {} // Objeto vazio
 let caneta ={
     cor: 'preto',
-    marca: 'BIC'
+    marca: 'BIC',
+    minhaCor: function () { 
+        return this.cor
+    },
+    escrever: function () { 
+        return '........escrevendo........'
+    }
 }
 caneta.ponta = 0.7 // atribuindo valor a caneta
 let propriedade = 'transparente'
 caneta[propriedade] = true
 console.log(caneta) // impriminto a caneta
-document.write(caneta.ponta) // retorna as propriedades da caneta
+// document.write(caneta.ponta) // retorna as propriedades da caneta
+// document.write(caneta.minhaCor())
+// document.write(caneta.escrever())
 
+/**
+ * Funções
+ * Representa ações ou métodos
+ */
+function quadrado2 (num) {
+    return num * num
+}
+function soma (n1, n2) {
+    return n1 + n2
+}
+document.write(quadrado2(4) + '<br>')
+document.write(soma(4, 3) + '<br>')
 
+/** Expressão de função */
+let subtracao = function (n1, n2) {
+    return n1 - n2
+}
+document.write(subtracao(10, 5))
+
+/** Arrow Function (Função de seta) */
+let quadrado = num => num * 2
+
+document.write(quadrado(10))
--- app/gameBabel.js
+++ app/gameBabel.js
@@ -12,13 +12,50 @@
 //  let obj = {} // Objeto vazio
 var caneta = {
   cor: 'preto',
-  marca: 'BIC'
+  marca: 'BIC',
+  minhaCor: function minhaCor() {
+    return this.cor;
+  },
+  escrever: function escrever() {
+    return '........escrevendo........';
+  }
 };
 caneta.ponta = 0.7; // atribuindo valor a caneta
 
 var propriedade = 'transparente';
 caneta[propriedade] = true;
 console.log(caneta); // impriminto a caneta
+// document.write(caneta.ponta) // retorna as propriedades da caneta
+// document.write(caneta.minhaCor())
+// document.write(caneta.escrever())
 
-document.write(caneta.ponta); // retorna as propriedades da caneta
+/**
+ * Funções
+ * Representa ações ou métodos
+ */
+
+function quadrado2(num) {
+  return num * num;
+}
+
+function soma(n1, n2) {
+  return n1 + n2;
+}
+
+document.write(quadrado2(4) + '<br>');
+document.write(soma(4, 3) + '<br>');
+/** Expressão de função */
+
+var subtracao = function subtracao(n1, n2) {
+  return n1 - n2;
+};
+
+document.write(subtracao(10, 5));
+/** Arrow Function (Função de seta) */
+
+var quadrado = function quadrado(num) {
+  return num * 2;
+};
+
+document.write(quadrado(10));
 //# sourceMappingURL=gameBabel.js.map
