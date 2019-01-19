c3fbadd4792bc663065664b55f0c445725faa181
LuizFelipems <luizfelipems12@gmail.com>
2019-01-18 23:01:45 -0300

    Sobre Objetos

--- app-src/gameBabel.js
+++ app-src/gameBabel.js
@@ -1,175 +1,21 @@
-// document.write("Fui compilado pelo Babel 7 ")
-
 /**
- * Variáveis
- * Você usa variáveis como nomes simbólicos para os valores em sua aplicação.
+ * Objeto
+ * São para modelar os objetos do mundo real.
+ * Possuem propriedades e funções.
  * 
- * Tipos de dados:
- * Boolean. true e false.
- * null. Uma palavra-chave que indica valor nulo.
- * undefined. Uma propriedade superior cujo valor é indefinido.
- * Number. 42 ou 3.14159.
- * String. "Howdy"
- * Symbol (novo em ECMAScript 6). Um tipo de dado cuja as instâncias são únicas e imutáveis.
- * Object. Objetos
- */
- var a = 1
- /** let - permite que você declare variáveis limitando seu escopo no bloco*/
- let aa = "1"
- {
-     let ab = "2" // ela respeita o escopo, só podendo ser acessada aqui dentro
- }
-//  document.write(aa)
-//  document.write(ab) // Uncaught ReferenceError: ab is not defined
-
- /** Convertendo strings para números */
-//  document.write(parseInt(aa))
-//  document.write(parseFloat(aa))
-
-/**
- * Hoisting (lançada para o topo)
- * É quando você utiliza a variável e declara depois, sem obter uma exceção.
- * Elas retornarão um valor undefined.
- */
-//  document.write(b)
-//  var b = 1
-
-/**
- * Constantes
- * Uma constante não pode alterar seu valor por meio de uma atribuição ou 
- * ao ser declarada novamente enquanto o script é executado. 
- * Deve ser inicializada com um valor.
- */
- const c = 1
-//  c = 2 // Erro de sintaxe, e c é somente leitura
-
-/**
- * Números
- * O objeto JavaScript Number é um objeto encapsulado que permite você trabalhar com valores numéricos.
- */
- let num = 1.123123
- let num2 = num.toFixed(2) // Retorna uma string representando o número na notação de ponto fixo.
-//  document.write(num2) 
-
- let n1 = 2
- let n2 = '3'
- let n3 = 'ab'
-//  document.write(n1/n2) // Aqui o js faz a conversão inplicitamente
-//  document.write(n1+n2) // Aqui o js faz a concatenação de strings 
-//  document.write(n1*n3) // Aqui o returna NaN
-//  document.write(n1 * Number(n2)) // Aqui convertemos a string para número
-//  document.write(n1 * +n2) // Aqui convertemos a string para número positivo
-//  document.write(n1 * -n2) // Aqui convertemos a string para número positivo
-
-
-/**
- * Strings
- * Strings são úteis para guardar dados que podem ser representados em forma de texto.
+ * *OBS: Como o JavaScript é uma linguagem prototipada, 
+ * não é necessário criar uma classe para termos um objeto.
  * 
  */
-//  let nome = 'Felipe "Ok"' // para imprimir as aspas no body
-//  let nome = 'Felipe \"Ok\" ' // para imprimir as aspas no body
- let nome = 'Luiz'
- let sNome = 'Felipe'
- let completo = nome + sNome
-//  document.write(nome + ' ' + sNome + ' Possui tamaho: ' + completo.length + '<br>') // retorna o tamanho da string
-//  document.write(completo.charAt(2) + '<br>') // Retorna o caracter da posição passada
-//  document.write(completo.substring(0, 3) + '<br>') // Retorna um trecho dessa string
-//  document.write(completo.toLowerCase() + '<br>') // Retorna a string em minúscula
-//  document.write(completo.toUpperCase() + '<br>') // Retorna a string em minúscula
- 
-/**
- * Boolean
- * true ou false
- * 
- * Se o valor é omitido ou é 0, -0, null, false, NaN, undefined "", 
- * o objeto terá um valor inicial de false.
- * 
- */
- let ligado = true
-//  document.write(ligado)
- 
-/**
- * Operadores
- * 
- * Aritméticos: +  -  *  /
- * Comparação: >  <  >=  <=
- * Igualdade: ==   ===(estritamente igual)  !=   !==(estritamente diferente)
- * Lógicos: &&  ||  !
- * Ternário: (expressao) ? se_true : se_false
- */
-//  document.write(`Aritméticos:  ${5 + 1}, ${5 - 1}, ${5 * 1}, ${10 / 2}; <br>
-//                 Comparação:  ${5 > 1}, ${5 < 1}, ${5 >= 1}, ${10 <= 2}; <br>
-//                 Igualdade:  ${10 == 10}, ${10 === '10'}, ${5 != 10}, ${10 != '10'}, ${10 !== '10'}; <br>
-//                 Lógicos:  ${5 > 1 && 5 < 10}, ${5 > 10 || 5 < 10}, ${!5 >= 1}; <br>
-//                 Ternário:  ${5 < 1 ? "Maior": "Menor"}; <br>
-//                 ` 
-//  )
+//  let obj = {} // Objeto vazio
+let caneta ={
+    cor: 'preto',
+    marca: 'BIC'
+}
+caneta.ponta = 0.7 // atribuindo valor a caneta
+let propriedade = 'transparente'
+caneta[propriedade] = true
+console.log(caneta) // impriminto a caneta
+document.write(caneta.ponta) // retorna as propriedades da caneta
 
 
-/**
- * Array
- * Semelhante a listas
- */
- let lista = ['leite', 'água', 'banana']
-//  lista[3] = 'farofa'
-//  document.write(lista.length) // Retorna o tamanho do array
-//  document.write(lista[1]) // Acessa o elemento do índice passado
-
-// lista.pop() // Retira o ultimo elemento do array
-// lista.shift() // Retira o primeiro elemento do array
-// lista.unshift('pedra') // Adiciona um elemento no início do array
-// lista.push('pedra') // Adiciona um elemento no final do array
-// lista.splice(1, 2) // Retira o elemento a partir do indice e a quantidade de elementos após ele
-//  document.write(lista) // Retorna o array
-
-
- /**
-  * Loops
-  * São laços de condição, que serão executados até que a operação seja atendida
-  * 
-  */
-//  let lista2 = ['leite', 'água', 'banana', 'carne', 'arroz', 'fejão', 'mostarda', 'macarrão', 'pão']
-
-//  let i = 0
-//  while (i <= 10) {
-//     document.write(i + '<br>')
-//     i+=1
-//  }
-
-//  for (let i = 0; i < lista2.length; i++) {
-//      document.write(lista2[i] + '<br>')
-//  }
-
-/**
- * Estrutura Condicional
- * if else
- * switch case
- */
- let idade = 22
- if (idade === 22) {
-    document.write('Você tem 22 anos!')
- } else if (idade >= 18) {
-    document.write('Maior de idade.')
- } else {
-    document.write('Menor de idade.')
- }
-
- switch (idade) {
-     case 17:
-         document.write('tem 17' + '<br>')
-         break;
-     case 18:
-         document.write('tem 18' + '<br>')
-     default:
-         document.write('Não sei')
-         break;
- }
-
- for (let i = 0; i < 10; i++) {
-     if (i === 3) {
-        //  break // Ele para o loop quando a condição for atingida
-         continue // Reinicia o loop quando a condição for atingida
-     }
-     document.write(i + '<br>')
- }
