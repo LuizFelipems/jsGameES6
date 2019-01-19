0e8c3d17e01aa1296dd5333caeb1eb221a8116a8
LuizFelipems <luizfelipems12@gmail.com>
2019-01-19 03:08:17 -0300

    Sobre: Renderização, Loop principal do jogo. Jogo Finalizado!

--- app-src/gameBabel.js
+++ app-src/gameBabel.js
@@ -19,7 +19,7 @@ const heroImage = new Image()
 heroImage.onload = function () { // Carrega a imagem do Heroi
     heroReady = true // Quando o Herói for carregado, ele está pronto para ser lido
 }
-heroImage.src = 'images/hero.png' // Atribui o src da imagem do Heroi
+heroImage.src = 'images/mario.png' // Atribui o src da imagem do Heroi
 
 /** Imagem do Monstro */
 let monsterRead = false
@@ -27,7 +27,7 @@ const monsterImage = new Image()
 monsterImage.onload = function () { // Carrega a imagem do Monstro
     monsterRead = true // Quando o Monstro for carregado, ele está pronto para ser lido
 }
-monsterImage.src = 'images/monster.png' // Atribui o src da imagem do Monstro
+monsterImage.src = 'images/star.png' // Atribui o src da imagem do Monstro
 
 /** Objetos do jogo */
 const hero = { // Objeto do Heroi
@@ -73,13 +73,13 @@ const update = function (modifier) {
     if (38 in keysDown) { // seta para cima
         hero.y -= hero.speed * modifier // posição Y: é a posição Y - (velocidade do heroi * 1000)
     }
-    if (40 in keyDown) { // seta para baixo
+    if (40 in keysDown) { // seta para baixo
         hero.y += hero.speed * modifier // posição Y: é a posição Y + (velocidade do heroi * 1000)
     }
-    if (37 in keyDown) { // seta para esquerda
+    if (37 in keysDown) { // seta para esquerda
         hero.x -= hero.speed * modifier // posição X: é a posição X - (velocidade do heroi * 1000)
     }
-    if (39 in keyDown) { // seta para direita
+    if (39 in keysDown) { // seta para direita
         hero.x += hero.speed * modifier // posição X: é a posição X + (velocidade do heroi * 1000)
     }
 
@@ -95,3 +95,44 @@ const update = function (modifier) {
     }
 }
 
+/** Renderiza tudo */
+const render = function () {
+    if (bgReady) { // Se o backgraund foi carregado 
+        ctx.drawImage(bgImage, 0, 0) // Desenha o backgraund na tela
+    }
+    if (heroReady) { // Se o herói foi carregado 
+        ctx.drawImage(heroImage, hero.x, hero.y) // Desenha o herói na tela
+    }
+    if (monsterRead) { // Se o monstro foi carregado 
+        ctx.drawImage(monsterImage, monster.x, monster.y) // Desenha o monstro na tela
+    }
+
+    /** Pontuação */
+    ctx.fillStyle = 'rgb(250, 250, 250)'
+    ctx.font = '24px Helvetica'
+    ctx.textAlign = 'left'
+    ctx.textBaseline = 'top'
+    ctx.fillText('Monstros pegos: ' + monstersCaught, 32, 32)
+}
+
+/** Controla o loop do jogo */
+const main = function () {
+    const now = Date.now() // Data agora em milisegundos
+    const delta = now - then // delta = (agora em milisegundos - Então(then)) 
+
+    update(delta / 1000)
+    render()
+
+    then = now
+
+    // Executa isso o mais breve possível
+    requestAnimationFrame(main) // Faz o trabalho de chamar o main toda vez que o jogo finalizar
+}
+
+// Suporte cross-browser para requestAnimationFrame
+const w = window
+const requestAnimationFrame = w.requestAnimationFrame || w.webkitRequestAnimationFrame || w.msRequestAnimationFrame || w.mozRequestAnimationFrame
+
+let then = Date.now() // Então(then), terá a data em milisegundo de quando o jogo iniciar 
+reset()
+main()
--- app/gameBabel.js
+++ app/gameBabel.js
@@ -30,7 +30,7 @@ heroImage.onload = function () {
   heroReady = true; // Quando o Herói for carregado, ele está pronto para ser lido
 };
 
-heroImage.src = 'images/hero.png'; // Atribui o src da imagem do Heroi
+heroImage.src = 'images/mario.png'; // Atribui o src da imagem do Heroi
 
 /** Imagem do Monstro */
 
@@ -42,7 +42,7 @@ monsterImage.onload = function () {
   monsterRead = true; // Quando o Monstro for carregado, ele está pronto para ser lido
 };
 
-monsterImage.src = 'images/monster.png'; // Atribui o src da imagem do Monstro
+monsterImage.src = 'images/star.png'; // Atribui o src da imagem do Monstro
 
 /** Objetos do jogo */
 
@@ -96,17 +96,17 @@ var update = function update(modifier) {
     hero.y -= hero.speed * modifier; // posição Y: é a posição Y - (velocidade do heroi * 1000)
   }
 
-  if (40 in keyDown) {
+  if (40 in keysDown) {
     // seta para baixo
     hero.y += hero.speed * modifier; // posição Y: é a posição Y + (velocidade do heroi * 1000)
   }
 
-  if (37 in keyDown) {
+  if (37 in keysDown) {
     // seta para esquerda
     hero.x -= hero.speed * modifier; // posição X: é a posição X - (velocidade do heroi * 1000)
   }
 
-  if (39 in keyDown) {
+  if (39 in keysDown) {
     // seta para direita
     hero.x += hero.speed * modifier; // posição X: é a posição X + (velocidade do heroi * 1000)
   }
@@ -120,4 +120,53 @@ var update = function update(modifier) {
     reset(); // Volta do começo
   }
 };
+/** Renderiza tudo */
+
+
+var render = function render() {
+  if (bgReady) {
+    // Se o backgraund foi carregado 
+    ctx.drawImage(bgImage, 0, 0); // Desenha o backgraund na tela
+  }
+
+  if (heroReady) {
+    // Se o herói foi carregado 
+    ctx.drawImage(heroImage, hero.x, hero.y); // Desenha o herói na tela
+  }
+
+  if (monsterRead) {
+    // Se o monstro foi carregado 
+    ctx.drawImage(monsterImage, monster.x, monster.y); // Desenha o monstro na tela
+  }
+  /** Pontuação */
+
+
+  ctx.fillStyle = 'rgb(250, 250, 250)';
+  ctx.font = '24px Helvetica';
+  ctx.textAlign = 'left';
+  ctx.textBaseline = 'top';
+  ctx.fillText('Monstros pegos: ' + monstersCaught, 32, 32);
+};
+/** Controla o loop do jogo */
+
+
+var main = function main() {
+  var now = Date.now(); // Data agora em milisegundos
+
+  var delta = now - then; // delta = (agora em milisegundos - Então(then)) 
+
+  update(delta / 1000);
+  render();
+  then = now; // Executa isso o mais breve possível
+
+  requestAnimationFrame(main); // Faz o trabalho de chamar o main toda vez que o jogo finalizar
+}; // Suporte cross-browser para requestAnimationFrame
+
+
+var w = window;
+var requestAnimationFrame = w.requestAnimationFrame || w.webkitRequestAnimationFrame || w.msRequestAnimationFrame || w.mozRequestAnimationFrame;
+var then = Date.now(); // Então(then), terá a data em milisegundo de quando o jogo iniciar 
+
+reset();
+main();
 //# sourceMappingURL=gameBabel.js.map
